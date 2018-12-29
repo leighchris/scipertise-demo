61b73214439c5a7cf6643ea824a8a4fa52241014
Leigh Christopher <leigh.christopher2@gmail.com>
2018-12-28 21:44:10 -0500

    Changed settings

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'c5guhld^0gtd%!dj6t7y1(o97o=a_j1d4^amtc(cn*g1qf#_bz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
