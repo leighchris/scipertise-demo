438d69ab4c3a267ed96e7f5f6dfb6ce8fd776d37
Leigh Christopher <leigh.christopher2@gmail.com>
2018-12-30 16:05:46 -0500

    added email backend to settings

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -176,11 +176,13 @@ LOGOUT_REDIRECT_URL = 'home'
 
 TAGGIT_CASE_INSENSITIVE = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'EMAIL_HOST_USER'
 EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASSWORD'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 
 
 # Activate Django-Heroku.
