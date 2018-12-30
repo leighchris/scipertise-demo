8a0037476ede2f86b853c589c1e45a2a178d20f3
Leigh Christopher <leigh.christopher2@gmail.com>
2018-12-30 16:31:19 -0500

    added setting email_backend

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -176,11 +176,11 @@ LOGOUT_REDIRECT_URL = 'home'
 
 TAGGIT_CASE_INSENSITIVE = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'EMAIL_HOST_USER'
 EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASSWORD'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'founders@scipertise.com'
 
