0ae3835ed9afa46bc68c9e04720fccd760eef6bb
Leigh Christopher <leigh.christopher2@gmail.com>
2019-01-02 15:08:25 -0500

    changed debug to ture

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
