1667043ad3ad9d98b9198d4e531e1e4d26343241
Leigh Christopher <leigh.christopher2@gmail.com>
2019-01-03 12:09:16 -0500

    changed settings from medium post

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -14,8 +14,8 @@ import os
 import django_heroku
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES[‘default’].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES[‘default’].update(db_from_env)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -118,11 +118,11 @@ WSGI_APPLICATION = 'scipertise_demo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-#DATABASES = {
-#    'default': dj_database_url.config(
-#        default=config('DATABASE_URL')
-#    )
-#}
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 DATABASES = {
     'default': {
