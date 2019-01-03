ff1856f8a813144e23ed7e0b0463cc03443eeae9
Leigh Christopher <leigh.christopher2@gmail.com>
2019-01-03 12:20:04 -0500

    changed settings from medium post

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -118,11 +118,11 @@ WSGI_APPLICATION = 'scipertise_demo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=config('DATABASE_URL')
+#    )
+#}
 
 DATABASES = {
     'default': {
