a997e38b8daa12e0e62ffe7c7726371bafab8984
Leigh Christopher <leigh.christopher2@gmail.com>
2019-01-03 12:05:25 -0500

    changed settings from medium post

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-import dj-database-url
+import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES[‘default’].update(db_from_env)
