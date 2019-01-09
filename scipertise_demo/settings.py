6e0478758e801f1dfd164a7cf68ed53c07f9194e
Leigh Christopher <leigh.christopher2@gmail.com>
2019-01-09 13:57:08 -0500

    added media configuration with amazon s3

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'bootstrap_datepicker_plus',
     'widget_tweaks',
     'sendgrid',
+    'storages',
  
   
  
@@ -183,8 +184,25 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
 MEDIA_URL = "/media/"
 #MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
