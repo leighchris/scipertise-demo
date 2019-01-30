38158296bdfe1d36765c9bf23e8a9a196617165d
Leigh Christopher <leigh.christopher2@gmail.com>
2019-01-30 14:54:22 -0500

    commented out Twilio code

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, re_path
-from . import views
-
-urlpatterns = [
-    re_path(r'^$', views.app, name='twilio'),
-    re_path(r'^token', views.token, name='token'),
-]
+#from django.urls import path, re_path
+#from . import views
+#
+#urlpatterns = [
+#    re_path(r'^$', views.app, name='twilio'),
+#    re_path(r'^token', views.token, name='token'),
+#]
